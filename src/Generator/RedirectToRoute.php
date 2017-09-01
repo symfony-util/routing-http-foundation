f02e49b3134af5ccb37b3d568b58131f215b4bf3
Nardberjean <Nardberjean@users.noreply.github.com>
2017-09-01 07:03:14 -0400

    Update RedirectToRoute.php

--- src/Generator/RedirectToRoute.php
+++ src/Generator/RedirectToRoute.php
@@ -15,6 +15,7 @@ namespace SymfonyUtil\Component\RoutingHttpFoundation\Generator;
 // https://github.com/symfony/symfony/tree/v3.3.8/src/Symfony/Component/Routing/Generator
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use SymfonyUtil\Component\HttpFoundation\ReRouteControllerModelInterface;
 use SymfonyUtil\Component\HttpFoundation\ResponseParameters;
